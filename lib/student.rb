dd4cdc40543a6dc4e0412f6ef6ff8c1a2149ea5d
Yonnick <yonnick00@gmail.com>
2019-04-16 17:30:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ def initialize(name, grade, id=nil)
 end
 
 def self.create_table
-  
+
 end
 
 
@@ -23,14 +23,14 @@ def initialize(name, album, id=nil)
 end
 
 def self.create_table
-  sql =  <<-SQL 
+  sql =  <<-SQL
     CREATE TABLE IF NOT EXISTS songs (
-      id INTEGER PRIMARY KEY, 
-      name TEXT, 
+      id INTEGER PRIMARY KEY,
+      name TEXT,
       album TEXT
       )
       SQL
-  DB[:conn].execute(sql) 
+  DB[:conn].execute(sql)
 end
 
 end
