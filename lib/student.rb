dd10ed936c22868f8c7574657962dfaa13365191
Yonnick <yonnick00@gmail.com>
2019-04-16 17:30:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,6 +3,34 @@ class Student
 attr_accessor :name, :grade, :db
 attr_reader :id
 
+def initialize(name, grade, id=nil)
+  @name = name
+  @grade = grade
+  @id = id
+end
 
+def self.create_table
+  
+end
+
+
+
+
+def initialize(name, album, id=nil)
+  @id = id
+  @name = name
+  @album = album
+end
+
+def self.create_table
+  sql =  <<-SQL 
+    CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      album TEXT
+      )
+      SQL
+  DB[:conn].execute(sql) 
+end
 
 end
